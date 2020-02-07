ce4ee0f131a5afdc930632e09fd8fdc7521bfc90
mohit99093 <43116791+mohit99093@users.noreply.github.com>
2020-02-07 23:50:13 +0530

    Lab_1

---
+++ Hadoop_Lab1/Question 2/ImageCounterMapper.java
@@ -0,0 +1,32 @@
+package com.example.lib;
+
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+import javax.naming.Context;
+
+public class ImageCounterMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+    @Override
+    public void map(LongWritable key, Text value, Context context)
+            throws IOException, InterruptedException {
+
+        String line = value.toString();
+        String imageType;
+        if (line.contains("GET") && line.contains("images")) {
+            if (line.contains(".jpg")) {
+                imageType = "JPG";
+            } else if (line.contains(".gif")) {
+                imageType = "GIF";
+            } else {
+                imageType = "OTHERS";
+            }
+            context.write(new Text(imageType), new IntWritable(1));
+        }
+    }
+}
---
+++ Hadoop_Lab1/README.md
@@ -0,0 +1,11 @@
+Hadoop_Lab-1
+
+This Lab consists of Map Reduce programs on Hadoop. Performed on a single node.
+
+Question 1: Input file has the data of weather of last few years and mapreduced returns the yearly maximum temperature record.
+
+Question 2: Input file consists of web access log produced by a web server and the Map Reduce program counts the number of times GIF, JPG, and other image files that have been accessed by clients.
+
+Question 3: Input file consists of web access log produced by a web server and the the Map Reduce program outputs Total number of requests and Total download size on Monthly basis.
+
+Question 4: Input file consists of web access log produced by a web server and the the Map Reduce program lists Timestamp, URL for which http response status has been 404.
